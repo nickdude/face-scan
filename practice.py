e0be53d94bb645f7fade2789e48520a73b6d0ce4
Aaryadev <aaryadevc@gmail.com>
2024-03-27 21:23:47 +0530

    testing

--- practice.py
+++ practice.py
@@ -442,6 +442,32 @@ def POS_WANG(frames, fs):
             h = S[0, :] + (np.std(S[0, :]) / np.std(S[1, :])) * S[1, :]
             mean_h = np.mean(h)
             for temp in range(h.shape[1]):
+                h[0, temp] = (h[0, temp] - mean_h)
+            H[0, m:n] = H[0, m:n] + (h[0])
+    BVP = H
+    BVP = detrend(np.mat(BVP).H, 100)
+    BVP = np.asarray(np.transpose(BVP))[0]
+    b, a = signal.butter(1, [0.75 / fs * 2, 3 / fs * 2], btype='bandpass')
+    BVP = signal.filtfilt(b, a, BVP.astype(np.double))
+    return BVP
+  
+  
+def POS_WANG2(frames, fs):
+    WinSec = 32 / fs
+    RGB = _process_video(frames)
+    N = RGB.shape[0]
+    H = np.zeros((1, N))
+    l = math.ceil(WinSec * fs)
+
+    for n in range(N):
+        m = n - l
+        if m >= 0:
+            Cn = np.true_divide(RGB[m:n, :], np.mean(RGB[m:n, :], axis=0))
+            Cn = np.mat(Cn).H
+            S = np.matmul(np.array([[0, 1, -1], [-2, 1, 1]]), Cn)
+            h = S[0, :] + (np.std(S[0, :]) / np.std(S[1, :])) * S[1, :]
+            mean_h = np.mean(h)
+            for temp in range(h.shape[1]):
                 h[0, temp] = (h[0, temp] - mean_h) / np.std(h)
             H[0, m:n] = H[0, m:n] + (h[0])
     BVP = H
@@ -669,7 +695,8 @@ def receive_list():
                 [mhr, hrr, thr, co, map, 
                 hu, bv, tbw, bwp, bmi, bf] = calc_all_params(age, gender, weight, height, sysbp, diabp, heart_rate_bpm)
                 si = calculate_Stress_Index(wave, sampling_rate)
-                asth_rs = get_asthama_riskscore(wave)
+                wave2 = POS_WANG2(frames, sampling_rate)
+                asth_rs = get_asthama_riskscore(wave2)
                 
                 
                 print({
